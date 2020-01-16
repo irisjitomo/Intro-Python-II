b1166834a97c158cc8db0193e8789daf26160832
unknown <irisjitomo@yahoo.com>
2020-01-16 17:48:51 -0500

    mvp

--- src/room.py
+++ src/room.py
@@ -1,9 +1,13 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+from item import Item
 
 class Room:
     def __init__(self, name, description):
         self.name = name
         self.description = description
-    def __repr__(self):
-        return self.name + '\n' + self.description
+        self.item = []
+    # def __repr__(self):
+    #     return self.name + '\n' + self.description
+    def __str__(self):
+        return f'{self.name}\n{self.description}\n Room Items: {self.item}'
